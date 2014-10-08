ea10972fbc2714e50fbd732fbc48123119e800c2
Fabian Jakobs <fabian@c9.io>
2014-10-08 11:20:17 +0000

    fix https://github.com/c9/newclient/issues/4703

--- pubsub-service.js
+++ pubsub-service.js
@@ -1,19 +1,15 @@
 module.exports = function (vfs, options, register) {
     var Stream = require('stream');
-    var stream;
+    
+    var stream = new Stream();
+    stream.readable = true;
 
     register(null, {
         subscribe: function (callback) {
-            if (stream) return callback(null, { stream: stream });
-
-            stream = new Stream();
-            stream.readable = true;
             callback(null, { stream: stream });
         },
 
         publish: function(message) {
-            if (!stream)
-                console.error("PubSub stream not ready");
             stream.emit("data", message);
         }
     });
