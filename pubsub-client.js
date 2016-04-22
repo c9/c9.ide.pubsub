b20bdfb12aa700ad20524f36183c9d4f1c443be5
nightwing <amirjanyan@gmail.com>
2016-04-22 15:38:13 +0000

    backward compat

--- pubsub-client.js
+++ pubsub-client.js
@@ -13,8 +13,6 @@ define(function(require, exports, module) {
         var c9 = imports.c9;
         var ext = imports.ext;
 
-        var code = require("text!./pubsub-service.js");
-
         /***** Initialization *****/
 
         var plugin = new Plugin("Ajax.org", main.consumes);
@@ -23,13 +21,18 @@ define(function(require, exports, module) {
         var stream, api;
 
         var loaded = false;
-        function load(){
+        var oldVfs = false;
+        function load(oldVfs) {
             if (loaded) return;
             loaded = true;
 
             ext.loadRemotePlugin("pubsub", {
-                file: "c9.ide.pubsub/pubsub-service.js"
+                file: oldVfs ? undefined : "c9.ide.pubsub/pubsub-service.js",
+                code: oldVfs ? require("text!./pubsub-service.js") : undefined
             }, function(err, remote) {
+                if (!remote && !oldVfs)
+                    return load(true);
+                
                 if (!remote)
                     return console.error(err);
 
